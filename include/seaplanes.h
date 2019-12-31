c4fd9bc4b882e01bca4eb7fca530efc53f5bca43
Henrick Deschamps <henrick.deschamps@gmail.com>
2019-12-31 13:17:16 +0900

    Adding a common header for seaplanes integration ease

---
+++ include/seaplanes.h
@@ -0,0 +1,21 @@
+//
+// Created by Henrick Deschamps on 31/12/2019.
+//
+
+#ifndef SEAPLANES_SEAPLANES_H
+#define SEAPLANES_SEAPLANES_H
+
+#include <ITimeManagementPolicy.h>
+#include <LogicalProcessorAttribute.h>
+#include <LogicalProcessorCommon.h>
+#include <LogicalProcessorLogger.h>
+#include <LogicalProcessorObjectClass.h>
+#include <LogicalProcessorObjectInstance.h>
+#include <LogicalProcessorObjectInstancePublished.h>
+#include <LogicalProcessorObjectInstanceSubscribed.h>
+#include <ProtoLogicalProcessor.h>
+#include <SeaplanesTime.h>
+#include <TimeManagementPolicyFactory.h>
+#include <TimeManagementPolicyTimeStep.h>
+
+#endif // SEAPLANES_SEAPLANES_H
